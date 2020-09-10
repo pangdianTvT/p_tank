45f1472b507f7ff9b8350a351546427c581dfeef
pangdianTvT <wangxin_0929@outlook.com>
2020-09-10 16:15:12 +0800

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    public void test(){
+        System.out.println("1111");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
